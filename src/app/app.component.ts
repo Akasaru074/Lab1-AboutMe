cbad7becfe4d4fa1a9021f1c271cf65f9717e326
Ivan Morozov <vanyam.vvo@gmail.com>
2025-09-25 19:00:59 +1000

    name animation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Lab1-AboutMe';
+
+
+  ngOnInit(): void {
+    const interval = setInterval(()=>{
+      this.currentPrinted += this.myName[this.offsetIndex++];
+      if (this.offsetIndex >= this.offsets.length) clearInterval(interval);
+    }, 250);
+  }
+
+  offsets = [0.15, 3.35, 6.7, 10.1, 13.3, 16.65, 19.94, 23.25, 26.55, 29.75, 33.15, 36.45, 39.85];
+  offsetIndex = 0;
+
+  currentPrinted = "";
+  private myName = "Иван Морозов ";
+
+  
+
+
 }
