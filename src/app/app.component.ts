98a557e9cdf09fbab0e105ee02b7d3662ab76e89
Ivan Morozov <vanyam.vvo@gmail.com>
2025-09-29 21:50:14 +1000

    added photo, minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,16 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
 
     this.title.setTitle(this._title);
-
-    const interval = setInterval(()=>{
+    setTimeout(()=> {
+      const interval = setInterval(()=>{
       this.currentPrinted += this.myName[this.offsetIndex++];
       if (this.offsetIndex >= this.offsets.length) clearInterval(interval);
     }, 250);
+    }, this.timeout);
   }
+
+  timeout = 500;
+  interval = 250;
 
   offsets = [0.15, 3.35, 6.7, 10.1, 13.3, 16.65, 19.94, 23.25, 26.55, 29.75, 33.15, 36.45, 39.85];
   offsetIndex = 0;
